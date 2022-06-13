6ad06816075f003d39ff2d9608d1c080bf46bb9d
Yurii Dukhovnyi <y.dukhovnyi@gmail.com>
2022-06-13 22:09:37 +0300

    Glia Core SDK Release test.version

---
+++ Package.swift
@@ -0,0 +1,52 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "GliaSDK",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "GliaSDK",
+            targets: ["GliaSDK"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ReactiveSwift",
+            url: "https://github.com/salemove/glia-ios-podspecs/releases/download/1.0/ReactiveSwift.xcframework.zip",
+            checksum: "f1322d3e07b01a4f2b1329b7ed43494259fba740c666231422b373ec50dc1e7d"
+        ),
+        .binaryTarget(
+            name: "GliaCoreDependency",
+            url: "https://github.com/salemove/glia-ios-podspecs/releases/download/1.0/GliaCoreDependency.xcframework.zip",
+            checksum: "bc770dbc55b188884c128a77f4c8fddf6c872d24eeff274e410bd3206d125e77"
+        ),
+        .binaryTarget(
+            name: "TwilioVoice",
+            url: "https://github.com/salemove/ios-bundle/releases/download/0.33.0/TwilioVoice.xcframework.zip",
+            checksum: "34288e0876a8840fa51d3813046cf1f40a5939079bea23ace5afe6e752f12f9e"
+        ),
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/salemove/ios-bundle/releases/download/0.33.0/WebRTC.xcframework.zip",
+            checksum: "f76e410f608d96989ba0312e099697703a37b4414f8f46bb9e30c3d9b4291a52"
+        ),
+        .binaryTarget(
+            name: "SalemoveSDK",
+            url: "https://github.com/salemove/ios-bundle/releases/download/test.version/SalemoveSDK.xcframework.zip",
+            checksum: "653b0e41d536fe68311c090ff1963062f979ce1b0cf0fc2ab7cdb9a856246bf6"
+        ),
+        .target(
+            name: "GliaSDK",
+            dependencies: [
+                "SalemoveSDK",
+                "ReactiveSwift",
+                "GliaCoreDependency",
+                "TwilioVoice",
+                "WebRTC"
+            ]
+        )
+    ]
+)
